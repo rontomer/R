42c0b83f271c731b04c61c161f14feb3412d6981
rontomer <tomer.r@fiverr.com>
2017-10-28 23:11:42 +0300

    Update ui.R
    Change header, sidebar and body colors to Black

--- SNA/ui.R
+++ SNA/ui.R
@@ -16,20 +16,20 @@ tweet_mentions <- read.csv('tweet_mentions.csv')
 tweets <- read.csv('tweets.csv')
 
 ## join datasets
-tags <- inner_join(tweet_tags,tweet_mentions) %>% 
-        inner_join(tweets) %>% 
-        group_by(y=year(created_at),m=month(created_at),w=week(created_at)) %>% 
-        summarise(num_tweets=n()) %>% 
-        data.frame()
+search_tags <- inner_join(tweet_tags,tweet_mentions) %>% 
+                inner_join(tweets) %>% 
+                group_by(y=year(created_at),m=month(created_at),w=week(created_at)) %>% 
+                summarise(num_tweets=n()) %>% 
+                data.frame()
 
 ## get unique years data
-years <- unique(tags$y)
+years <- unique(search_tags$y)
 
 
 dashboardPage(
-    dashboardHeader(title = "Social Network Analysis",titleWidth = 350),
+    dashboardHeader(title = "Social Network Graph",titleWidth = 300),
     dashboardSidebar(
-        width=350,   
+        width=300,
         selectInput('year',label='Choose Year',multiple=F,selectize=T,choices = c('',years),selected = ''),
         uiOutput("month"),
         uiOutput("week"),
@@ -40,6 +40,34 @@ dashboardPage(
         )
     ),
     dashboardBody(
+        tags$head(
+            tags$style(
+                HTML('/* logo */
+                     .skin-blue .main-header .logo {
+                     background-color: black;
+                     }
+                     
+                     /* logo when hovered */
+                     .skin-blue .main-header .logo:hover {
+                     background-color: black;
+                     }
+                     
+                     /* navbar (rest of the header) */
+                     .skin-blue .main-header .navbar {
+                     background-color: black;
+                     }        
+                     
+                     /* main sidebar */
+                     .skin-blue .main-sidebar {
+                     background-color: black;
+                     }
+                     
+                     .content-wrapper,.right-side {
+                     background-color: black;
+                     }'
+                    )
+                )
+            ),
         tabItems(
             tabItem("Graph",
                     fluidRow(
